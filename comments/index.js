7b3884636006ab79f5b00381b87dfc41b7a19c5f
Johann Muller <developerjohannmuller@gmail.com>
2022-07-26 19:33:17 +0200

    Added React Client

--- comments/index.js
+++ comments/index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto')
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const commentsByPostId = {}
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || [])
-})
+});
 
 app.post('/posts/:id/comments', (req, res) => {
   const commentId = randomBytes(4).toString('hex')
@@ -21,8 +23,8 @@ app.post('/posts/:id/comments', (req, res) => {
   commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments)
-})
+});
 
 app.listen(4001, () => {
   console.log('Listening on 4001')
-})
+}); 
